b5ee205c2260e280d33a9096102e5cd24dd8f495
MKULTRA001 <tim.salazar001@gmail.com>
2021-07-14 00:16:44 -0600

    added js and changed buttons also css updates for faq

---
+++ Function.js
@@ -0,0 +1,23 @@
+function toggleCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+   
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    var prev_state_display = document.getElementById(cityName).style.display;
+    
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    if( prev_state_display === "none"){
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+    } else {
+        document.getElementById(cityName).style.display = "none";
+        evt.currentTarget.className.replace(" active", "");
+    }
+}
+document.getElementById("defaultOpen").click();
