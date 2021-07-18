6c9328278593cedbd3c1404d6e3c47ed86419a4c
MKULTRA001 <tim.salazar001@gmail.com>
2021-07-17 19:19:03 -0600

    fixed mobile compatibility issue

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ tl.to(".slider", {y: "-100%", duration: 1, stagger: 1.5, delay: 1});
 tl.to(".intro", {y: "-220%", duration: 1, stagger: 1.5}, "=-0.7");
 tl.call(removeElement(".introF"))
 tl.fromTo(".nav", {opacity: 0}, {opacity: 1, duration: 1});
-tl.fromTo(".cmain_logo", {opacity: 0}, {opacity: 1, duration: 1},("-=0.3"));
+tl.fromTo(".cmain_logo", {opacity: 0}, {opacity: 1, duration: 1},);
 tl.fromTo(".main_text", {opacity: 0}, {opacity: 1, duration: 1}, ("-=1"));
 tl.fromTo(".boxed", {opacity: 0}, {opacity: 1, duration: 1});
 tl.fromTo(".footer-basic", {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
--- app_c.js
+++ app_c.js
@@ -1,6 +1,7 @@
 document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.preventDefault();
-    e.target.elements.name.value = '';
+    var name = e.target.elements.name.value = '';
     e.target.elements.email.value = '';
     e.target.elements.message.value = '';
+    console.log('Using getAttribute: ' + e.target.elements.name.value('name'));
   });
