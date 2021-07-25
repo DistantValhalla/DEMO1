2db6b5dc3486cf64cb8425fc89366d15836289bf
MKULTRA001 <tim.salazar001@gmail.com>
2021-07-25 14:10:25 -0600

    Speed up animation and removed scaling from index to text mobile

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ tl.to(".intro", {y: "-220%", duration: 1, stagger: 1.5}, "=-0.7");
 tl.call(removeElement(".introF"));
 tl.fromTo(".nav", {opacity: 0}, {opacity: 1, duration: 1}, "-=0.7");
 tl.fromTo(".cmain_logo", {opacity: 0}, {opacity: 1, duration: 1},"-=0.2");
-tl.fromTo(".main_text", {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo(".main_text", {opacity: 0}, {opacity: 1, duration: 1},"-=1");
 tl.fromTo(".boxed", {opacity: 0}, {opacity: 1, duration: 1});
 tl.to(".hh_text", {opacity: 1},"-=1");
-tl.to(".slider-wrap", {opacity: 1},"-=0.5");
+tl.to(".slider-wrap", {opacity: 1},"-=1");
 tl.fromTo(".footer-basic", {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
 
 
